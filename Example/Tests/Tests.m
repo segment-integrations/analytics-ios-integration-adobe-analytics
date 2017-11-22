80df72b1452c5f06266e2341a72fd0bffdc5d029
Ladan Nasserian <ladanazita@users.noreply.github.com>
2017-11-21 16:30:50 -0800

    Implements ECommerce (#9)
    * Implements ECommerce
    	Adobe has a few reserved ecommerce events which Segment maps to.
    	Specifically `scAdd` for `Product Added`, `scRemove` for `Product Removed`
    	`scView` for `Cart Viewed`, `scCheckout` for `Checkout Started`, `prodView` for 
             `Product Viewed` and `purchase` for `Order Completed`.
    
    	Segment does not have a spec'd event for Adobe's `scOpen` event,
    	which has been described in Adobe as a "Carts" event. Segment
    	will not support sending this.
    
    	Adobe also expects products to be passed in with the key `&&products` and
    	as a String formatted like so:
    	`"Category;Product;Quantity;Price;eventN=X[|eventN2=X2];eVarN=merch_category[|eVarN2=merch_category2]"`
    
    	Product is a required argument, so if this is not present, Segment does not create the
    	`&&products` String. This value can be the product name, sku, or productId,
    	which is configured via the Segment setting `productIdentifier`.
    
    	If the other values in the String are missing, Segment
    	will leave the space empty but keep the `;` delimiter to preserve the order
    	of the product properties.
    
    	Segment does not strip all commas and semicolons from product and
    	category names, which aligns with the behavior of the analytics.js
    	Adobe-Analytics component. Segment documents that users should not
    	pass these characters in.
    
    	If `&&products` contains multiple products, the end of a product will
    	be delimited by a `,`.
    
    	Segment will also send in any additional `contextDataVariables` configured
    	in Segment settings.
    
    	Adobe allows you to send `&&products` via a `trackAction` or `trackState` call.
    	Since Segment does not spec sending ecommerce data on `screen`, Segment
    	will only support sending `&&products` on `track` as a `trackAction` call.
    
    	If a product-specific event is triggered, it must also be sent with the
    	`&&events` variable. Segment will send in the Adobe Ecommerce
    	event as the `&&events` variable.
    
    	This initial implementation does not support merchandising categories,
    	product-specific currency events, and order-wide currency events. This
    	will only support non-purchase and purchase events, while the additional
    	functionality will come in individual branches if we choose to support
    	them.

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -86,7 +86,7 @@ describe(@"SEGAdobeIntegration", ^{
             [verify(mockADBMobile) trackAction:@"myapp.testing" data:nil];
         });
 
-        it(@"tracks an action with properties NOT configured in settings.contextValues", ^{
+        it(@"tracks an action with nil properties if they are NOT configured in settings.contextValues", ^{
             integration = [[SEGAdobeIntegration alloc] initWithSettings:@{ @"eventsV2" : @{
                 @"Signed Up" : @"myapp.signedup",
                 @"Testing" : @"myapp.testing"
@@ -141,5 +141,223 @@ describe(@"SEGAdobeIntegration", ^{
             [verify(mockADBMobile) trackState:@"Sign Up" data:@{ @"myapp.new_user" : @YES }];
         });
     });
+
+    describe(@"ecommmerce", ^{
+        beforeEach(^{
+            integration = [[SEGAdobeIntegration alloc] initWithSettings:@{ @"productIdentifier" : @"name" }
+                                                           andADBMobile:mockADBMobile];
+        });
+
+        it(@"tracks Product Added with productIdentifier product_id", ^{
+            integration = [[SEGAdobeIntegration alloc] initWithSettings:@{ @"productIdentifier" : @"id" }
+                                                           andADBMobile:mockADBMobile];
+            SEGTrackPayload *trackPayload = [[SEGTrackPayload alloc] initWithEvent:@"Product Added" properties:@{
+                @"cart_id" : @"skdjsidjsdkdj29j",
+                @"product_id" : @"507f1f77bcf86cd799439011",
+                @"sku" : @"G-32",
+                @"category" : @"Games",
+                @"name" : @"Monopoly: 3rd Edition",
+                @"brand" : @"Hasbro",
+                @"variant" : @"200 pieces",
+                @"price" : @18.99,
+                @"quantity" : @1,
+                @"coupon" : @"MAYDEALS",
+                @"position" : @3,
+                @"url" : @"https://www.company.com/product/path",
+                @"image_url" : @"https://www.company.com/product/path.jpg"
+            }
+                context:@{}
+                integrations:@{}];
+            [integration track:trackPayload];
+            [verify(mockADBMobile) trackAction:@"scAdd" data:@{
+                @"&&events" : @"scAdd",
+                @"&&products" : @"Games;507f1f77bcf86cd799439011;1;18.99"
+            }];
+        });
+
+        it(@"tracks Product Added with productIdentifier falling back to id", ^{
+            integration = [[SEGAdobeIntegration alloc] initWithSettings:@{ @"productIdentifier" : @"id" }
+                                                           andADBMobile:mockADBMobile];
+            SEGTrackPayload *trackPayload = [[SEGTrackPayload alloc] initWithEvent:@"Product Added" properties:@{
+                @"cart_id" : @"skdjsidjsdkdj29j",
+                @"id" : @"507f1f77bcf86cd799439011",
+                @"sku" : @"G-32",
+                @"category" : @"Games",
+                @"name" : @"Monopoly: 3rd Edition",
+                @"brand" : @"Hasbro",
+                @"variant" : @"200 pieces",
+                @"price" : @18.99,
+                @"quantity" : @1,
+                @"coupon" : @"MAYDEALS",
+                @"position" : @3,
+                @"url" : @"https://www.company.com/product/path",
+                @"image_url" : @"https://www.company.com/product/path.jpg"
+            }
+                context:@{}
+                integrations:@{}];
+            [integration track:trackPayload];
+            [verify(mockADBMobile) trackAction:@"scAdd" data:@{
+                @"&&events" : @"scAdd",
+                @"&&products" : @"Games;507f1f77bcf86cd799439011;1;18.99"
+            }];
+        });
+
+        it(@"tracks Product Removed without properties", ^{
+            SEGTrackPayload *trackPayload = [[SEGTrackPayload alloc] initWithEvent:@"Product Removed" properties:@{}
+                context:@{}
+                integrations:@{}];
+            [integration track:trackPayload];
+            [verify(mockADBMobile) trackAction:@"scRemove" data:nil];
+        });
+
+        it(@"tracks Product Removed without quantity and price", ^{
+            SEGTrackPayload *trackPayload = [[SEGTrackPayload alloc] initWithEvent:@"Product Removed" properties:@{
+                @"cart_id" : @"skdjsidjsdkdj29j",
+                @"product_id" : @"507f1f77bcf86cd799439011",
+                @"sku" : @"G-32",
+                @"category" : @"Games",
+                @"name" : @"Monopoly: 3rd Edition",
+                @"brand" : @"Hasbro",
+                @"variant" : @"200 pieces",
+                @"coupon" : @"MAYDEALS",
+                @"position" : @3,
+                @"url" : @"https://www.company.com/product/path",
+                @"image_url" : @"https://www.company.com/product/path.jpg"
+            }
+                context:@{}
+                integrations:@{}];
+            [integration track:trackPayload];
+            [verify(mockADBMobile) trackAction:@"scRemove" data:@{
+                @"&&events" : @"scRemove",
+                @"&&products" : @"Games;Monopoly: 3rd Edition;1;0"
+            }];
+        });
+
+        it(@"does not track products on Cart Viewed without required product identifier", ^{
+            NSDictionary *props = @{
+                @"share_via" : @"email",
+                @"share_message" : @"Hey, check out this item",
+                @"recipient" : @"friend@gmail.com",
+                @"cart_id" : @"d92jd29jd92jd29j92d92jd",
+                @"products" : @[
+                    @{@"product_id" : @"507f1f77bcf86cd799439011"},
+                    @{@"product_id" : @"505bd76785ebb509fc183733"}
+                ]
+            };
+
+            SEGTrackPayload *trackPayload = [[SEGTrackPayload alloc] initWithEvent:@"Cart Viewed" properties:props
+                context:@{}
+                integrations:@{}];
+            [integration track:trackPayload];
+            [verify(mockADBMobile) trackAction:@"scView" data:nil];
+        });
+
+        it(@"tracks Product Viewed with product identifier as sku", ^{
+            integration = [[SEGAdobeIntegration alloc] initWithSettings:@{ @"productIdentifier" : @"sku" }
+                                                           andADBMobile:mockADBMobile];
+            NSDictionary *props = @{
+                @"product_id" : @"507f1f77bcf86cd799439011",
+                @"sku" : @"G-32",
+                @"category" : @"Food",
+                @"name" : @"Turkey",
+                @"brand" : @"Farmers",
+                @"variant" : @"Free Range",
+                @"price" : @180.99,
+                @"quantity" : @1,
+                @"coupon" : @"TURKEYDAY",
+                @"currency" : @"usd",
+                @"position" : @3,
+                @"value" : @20.99,
+                @"url" : @"https://www.company.com/product/path",
+                @"image_url" : @"https://www.company.com/product/path.jpg"
+            };
+
+            SEGTrackPayload *trackPayload = [[SEGTrackPayload alloc] initWithEvent:@"Product Viewed" properties:props
+                context:@{}
+                integrations:@{}];
+            [integration track:trackPayload];
+            [verify(mockADBMobile) trackAction:@"prodView" data:@{
+                @"&&events" : @"prodView",
+                @"&&products" : @"Food;G-32;1;180.99"
+            }];
+        });
+
+        it(@"tracks Checkout Started without category", ^{
+            NSDictionary *props = @{
+                @"checkout_id" : @"4230523091",
+                @"order_id" : @"13125032",
+                @"affiliation" : @"Google Play",
+                @"total" : @16.18,
+                @"tax" : @2.20,
+                @"currency" : @"USD",
+                @"revenue" : @8,
+                @"products" : @[
+                    @{
+                       @"product_id" : @"2013294",
+                       @"name" : @"Farmville",
+                       @"brand" : @"Zynga",
+                       @"price" : @"9.99",
+                       @"quantity" : @"1"
+                    },
+                    @{
+                       @"product_id" : @"149820",
+                       @"name" : @"Words With Friends",
+                       @"brand" : @"Zynga",
+                       @"price" : @"3.99",
+                       @"quantity" : @"1"
+                    }
+                ]
+            };
+            SEGTrackPayload *trackPayload = [[SEGTrackPayload alloc] initWithEvent:@"Checkout Started" properties:props
+                context:@{}
+                integrations:@{}];
+            [integration track:trackPayload];
+            [verify(mockADBMobile) trackAction:@"scCheckout" data:@{
+                @"&&events" : @"scCheckout",
+                @"&&products" : @";Farmville;1;9.99,;;Words With Friends;1;3.99"
+            }];
+        });
+
+        it(@"tracks Order Completed with products", ^{
+            NSDictionary *props = @{
+                @"checkout_id" : @"9bcf000000000000",
+                @"order_id" : @"50314b8e",
+                @"affiliation" : @"App Store",
+                @"total" : @30.45,
+                @"shipping" : @5.05,
+                @"tax" : @1.20,
+                @"currency" : @"USD",
+                @"category" : @"Games",
+                @"revenue" : @8,
+                @"products" : @[
+                    @{
+                       @"product_id" : @"2013294",
+                       @"category" : @"Games",
+                       @"name" : @"Monopoly: 3rd Edition",
+                       @"brand" : @"Hasbros",
+                       @"price" : @"21.99",
+                       @"quantity" : @"1"
+                    },
+                    @{
+                       @"product_id" : @"149820",
+                       @"category" : @"Games",
+                       @"name" : @"Battleship",
+                       @"brand" : @"Hasbros",
+                       @"price" : @"13.99",
+                       @"quantity" : @"2"
+                    }
+                ]
+            };
+
+            SEGTrackPayload *trackPayload = [[SEGTrackPayload alloc] initWithEvent:@"Order Completed" properties:props context:@{}
+                integrations:@{}];
+            [integration track:trackPayload];
+            [verify(mockADBMobile) trackAction:@"purchase" data:@{
+                @"&&events" : @"purchase",
+                @"&&products" : @"Games;Monopoly: 3rd Edition;1;21.99,;Games;Battleship;2;27.98"
+            }];
+        });
+    });
+
 });
 SpecEnd
--- Pod/Classes/SEGAdobeIntegration.m
+++ Pod/Classes/SEGAdobeIntegration.m
@@ -59,26 +59,21 @@
 
 - (void)track:(SEGTrackPayload *)payload
 {
-    NSMutableDictionary *data = [self mapContextValues:payload.properties];
-    NSString *event = [self mapEventsV2:payload.event];
-    if (!event) {
-        SEGLog(@"Event must be configured in Adobe and in the EventsV2 setting in Segment before sending.");
-        return;
-    }
-
-    [self.ADBMobile trackAction:event data:data];
-    SEGLog(@"[ADBMobile trackAction:%@ data:data];", payload.event, data);
+    [self realTrack:payload.event andProperties:payload.properties];
 }
 
 - (void)screen:(SEGScreenPayload *)payload
 {
     NSMutableDictionary *data = [self mapContextValues:payload.properties];
     [self.ADBMobile trackState:payload.name data:data];
-    SEGLog(@"[ADBMobile trackState:%@ data:data];", payload.name, data);
+    SEGLog(@"[ADBMobile trackState:%@ data:%@];", payload.name, data);
 }
 
 #pragma mark - Util Functions
 
+// All context data variables must be mapped by using processing rules,
+// meaning they must be configured as a context variable in Adobe's UI
+// and mapped from a Segment Property to the configured variable in Adobe
 - (NSMutableDictionary *)mapContextValues:(NSDictionary *)properties
 {
     NSInteger contextValuesSize = [self.settings[@"contextValues"] count];
@@ -104,6 +99,135 @@
         }
     }
     return nil;
+}
+
+- (NSMutableDictionary *)mapProducts:(NSString *)event andProperties:(NSDictionary *)properties
+{
+    if ([properties count] == 0) {
+        return nil;
+    }
+
+    NSMutableDictionary *data = [self mapContextValues:properties];
+    NSMutableDictionary *contextData = [[NSMutableDictionary alloc] initWithDictionary:data];
+
+    // If you trigger a product-specific event by using the &&products variable,
+    // you must also set that event in the &&events variable.
+    // If you do not set that event, it is filtered out during processing.
+    [contextData setObject:event forKey:@"&&events"];
+
+    NSString *formattedProducts = @"";
+    // If Products is of type NSArray (ex. Order Completed),
+    // we must end each product with a `,` to denote multiple products
+    NSArray *products = properties[@"products"];
+    if ([products isKindOfClass:[NSArray class]]) {
+        int count = 0;
+        for (NSDictionary *obj in products) {
+            // Catch the case where productIdentifier is nil
+            if ([self formatProducts:obj] == nil) {
+                return nil;
+            }
+            formattedProducts = [formattedProducts stringByAppendingString:[self formatProducts:obj]];
+            count++;
+            if (count < [products count]) {
+                formattedProducts = [formattedProducts stringByAppendingString:@",;"];
+            }
+        }
+    } else {
+        formattedProducts = [self formatProducts:properties];
+    }
+
+    [contextData setObject:formattedProducts forKey:@"&&products"];
+    return contextData;
+}
+
+// The `&&products` variable is expected to be formated in the following order:
+// `"Category;Product;Quantity;Price;eventN=X[|eventN2=X2];eVarN=merch_category[|eVarN2=merch_category2]"`
+// formatProducts can take in an object from the products array:
+//  @"products" : @[
+//      @{
+//          @"product_id" : @"2013294",
+//          @"category" : @"Games",
+//          @"name" : @"Monopoly: 3rd Edition",
+//          @"brand" : @"Hasbros",
+//          @"price" : @"21.99",
+//          @"quantity" : @"1"
+//      }
+// And output the following : @"Games;Monopoly: 3rd Edition;1;21.99,;Games;Battleship;2;27.98"
+//
+// It can also format a product passed in as a top level property, for example
+//      @{
+//          @"product_id" : @"507f1f77bcf86cd799439011",
+//          @"sku" : @"G-32",
+//          @"category" : @"Food",
+//          @"name" : @"Turkey",
+//          @"brand" : @"Farmers",
+//          @"variant" : @"Free Range",
+//          @"price" : @180.99,
+//          @"quantity" : @1,
+//      }
+//
+// And output the following: @"Food;G-32;1;180.99"
+
+- (NSString *)formatProducts:(NSDictionary *)obj
+{
+    NSString *category = obj[@"category"] ?: @"";
+
+    // The product argument is REQUIRED for Adobe ecommerce events.
+    // This value can be 'name', 'sku', or 'id'. Defaults to name
+    NSString *productIdentifier = obj[self.settings[@"productIdentifier"]];
+
+    // Fallback to id. Segment's ecommerce v1 Spec'd `id` as the product identifier
+    // The setting productIdentifier checks for id, where ecommerce V2
+    // is expecting product_id.
+    if ([self.settings[@"productIdentifier"] isEqualToString:@"id"]) {
+        productIdentifier = obj[@"product_id"] ?: obj[@"id"];
+    }
+
+    if ([productIdentifier length] == 0) {
+        NSLog(@"Product is a required field.");
+        return nil;
+    }
+
+
+    // Adobe expects Price to refer to the total price (unit price x units).
+    int quantity = [obj[@"quantity"] intValue] ?: 1;
+    double price = [obj[@"price"] doubleValue] ?: 0;
+    double total = price * quantity;
+
+    NSArray *output = @[ category, productIdentifier, [NSNumber numberWithInt:quantity], [NSNumber numberWithDouble:total] ];
+    return [output componentsJoinedByString:@";"];
+}
+
+- (void)realTrack:(NSString *)event andProperties:(NSDictionary *)properties
+{
+    NSDictionary *contextData;
+
+    // You can send ecommerce events via either a trackAction or trackState call.
+    // Since Segment does not spec sending products on `screen`, we
+    // will only support sending this via trackAction
+    NSDictionary *adobeEcommerceEvents = @{
+        @"Product Added" : @"scAdd",
+        @"Product Removed" : @"scRemove",
+        @"Cart Viewed" : @"scView",
+        @"Checkout Started" : @"scCheckout",
+        @"Order Completed" : @"purchase",
+        @"Product Viewed" : @"prodView"
+    };
+    if (adobeEcommerceEvents[event]) {
+        contextData = [self mapProducts:adobeEcommerceEvents[event] andProperties:properties];
+        [self.ADBMobile trackAction:adobeEcommerceEvents[event] data:contextData];
+        return;
+    }
+
+    NSMutableDictionary *data = [self mapContextValues:properties];
+    event = [self mapEventsV2:event];
+    if (!event) {
+        SEGLog(@"Event must be configured in Adobe and in the EventsV2 setting in Segment before sending.");
+        return;
+    }
+
+    [self.ADBMobile trackAction:event data:data];
+    SEGLog(@"[ADBMobile trackAction:%@ data:%@];", event, data);
 }
 
 @end
